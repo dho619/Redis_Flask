2b209d7e419c63b83513183705494543e5bb3d6e
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2020-04-23 17:11:13 -0300

    Tirando parte de código desnecessária

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api
-from os import curdir
-from os.path import abspath
 
 #importar os Resources, que estao na outra pasta
 from Resources.Login import Login
 from Resources.Values import Values
 from Resources.Keys import Keys
 
-abspath(curdir)#adicionar esse diretorio para achar os modulos em subpastas
 
 #passando a quem pertence a instancia de Flask
 app = Flask(__name__)
